8984a05915f356d62ef04f25356b1c72c8c17dfe
kchanin@bitbucket.org <kchanin@gmail.com>
2017-02-14 20:00:36 -0500

    Pokemon class created

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Ari Chanin on 2/14/17.
+//  Copyright © 2017 kbob. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon{
+    
+    private var _name: String!
+    private var _pokedexID: Int!
+    
+    var name: String{
+        return _name
+    }
+    
+    var pokedexID: Int{
+        return _pokedexID
+    }
+    
+    init(name: String, pokedexID:Int){
+        self._name = name
+        self._pokedexID = pokedexID
+    }
+}
